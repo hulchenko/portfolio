d5d78726eaebbce6a6e23d407e5c1d3d2db0be64
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-04-22 00:30:08 -0600

    update dynamic shadow

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const themeToggle = document.querySelector('#theme-toggle');
 const container = document.querySelector('.container');
 const toggle = document.querySelector('.toggle');
 const buttonUp = document.querySelector('.button-up');
-const greeting = document.querySelector('.greeting');
 const navDisable = document.querySelector('.fa-times');
 const navigation = document.querySelector('.navigation');
 const logo = document.querySelector('.logo');
+const intro = document.querySelector('.intro');
+const greeting = document.querySelector('.greeting');
 
 //Set Dark theme
 themeToggle.addEventListener('click', () => {
@@ -35,16 +36,6 @@ document.querySelector('#home').addEventListener('click', () => {
 });
 
 //Typed Animation
-const typed = new Typed(greeting, {
-  strings: [
-    'Hello!^2000<br>My name is Vadym^1000<br>and I love coding!',
-    'Hello!<br>Welcome to My Portfolio!',
-  ],
-  typeSpeed: 90,
-  backSpeed: 90,
-  smartBackspace: true,
-  showCursor: false,
-});
 
 //Alternative Version
 // var i = 0;
@@ -62,3 +53,35 @@ const typed = new Typed(greeting, {
 // window.onload = function () {
 //   typeWriter();
 // };
+
+const typed = new Typed(greeting, {
+  strings: [
+    'Hello!^2000<br>My name is Vadym^1000<br>and I love coding!',
+    'Hello!<br>Welcome to My Portfolio!',
+  ],
+  typeSpeed: 90,
+  backSpeed: 90,
+  smartBackspace: true,
+  showCursor: false,
+});
+
+//Dynamic Shadow
+const distance = 15;
+function shadow(e) {
+  let height = intro.offsetHeight;
+  let width = intro.offsetWidth;
+
+  let x = e.offsetX;
+  let y = e.offsetY;
+
+  if (this !== e.target) {
+    //make shadow align with the title on hover
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  const xDistance = Math.round((x / width) * distance - distance / 2);
+  const yDistance = Math.round((y / height) * distance - distance / 2);
+  greeting.style.textShadow = `${xDistance}px ${yDistance}px 20px #fff`;
+}
+intro.addEventListener('mousemove', shadow);
