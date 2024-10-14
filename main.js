26af94603d3e818a2c97b4dbf4dceef4c9553b52
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-10-14 03:26:20 -0300

    move js files into components dir

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import { loadSkills } from "./skills.js";
-import { loadProjects } from "./projects.js";
-import { useProfilePicToggle } from "./img-toggle.js";
-import { useTheme } from "./theme.js";
-import { useCarousel } from "./carousel.js";
-import { loadIntro } from "./intro.js";
-import { useNav } from "./nav.js";
+import { loadSkills } from "./components/skills.js";
+import { loadProjects } from "./components/projects.js";
+import { useProfilePicToggle } from "./components/img-toggle.js";
+import { useTheme } from "./components/theme.js";
+import { useCarousel } from "./components/carousel.js";
+import { loadIntro } from "./components/intro.js";
+import { useNav } from "./components/nav.js";
 
 // render data
 loadIntro();
