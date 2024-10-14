92ab65438326bb4f3ff2c33794c6a81e21bdec0c
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-10-14 00:44:32 -0300

    rename product files; rename imgs dir to images

--- img-toggle.js
+++ img-toggle.js
@@ -3,9 +3,9 @@ const profilePic = document.querySelector("#profile-pic");
 const toggle = () => {
   profilePic.src.includes("solo")
     ? (profilePic.src =
-        "https://hulchenko.github.io/portfolio/imgs/together-img.jpg")
+        "https://hulchenko.github.io/portfolio/images/profile/together-img.jpg")
     : (profilePic.src =
-        "https://hulchenko.github.io/portfolio/imgs/solo-img.jpg");
+        "https://hulchenko.github.io/portfolio/images/profile/solo-img.jpg");
 };
 
 export const useProfilePicToggle = () => {
--- projects.js
+++ projects.js
@@ -1,35 +1,45 @@
+// TODO
 const carousel = document.getElementById("carousel-container");
 
 const projects = [
   {
-    src: "./imgs/carousel/1.jpg",
-    title: "BoomBox App",
-    tools: "React, SASS",
+    src: "./images/carousel/calorie-tracker.png",
+    title: "Calorie Tracker",
+    tools: "TypeScript, NextJS, Tailwind, Neon PostgreSQL, Chakra UI",
     description:
-      "Play/pause/skip/skip-back/auto-play functionality. Nav bar 'Library', with responsive on-click functionality to switch songs. Mobile friendly.",
-    code: "https://github.com/hulchenko/react-music-app",
-    demo: "https://kind-aryabhata-44a21e.netlify.app/",
+      "Allows you to track your daily and weekly calorie intake and manage your health goals. Auth control with profile management.",
+    code: "https://github.com/hulchenko/nextjs-calorie-tracker",
+    demo: "https://nextjs-calorie-tracker-woad.vercel.app/",
   },
+  // {
+  //   src: "./images/carousel/boombox.jpg",
+  //   title: "BoomBox App",
+  //   tools: "React, SASS",
+  //   description:
+  //     "Play/pause/skip/skip-back/auto-play functionality. Nav bar 'Library', with responsive on-click functionality to switch songs. Mobile friendly.",
+  //   code: "https://github.com/hulchenko/react-music-app",
+  //   demo: "https://kind-aryabhata-44a21e.netlify.app/",
+  // },
+  // {
+  //   src: "./images/carousel/file-upload.jpg",
+  //   title: "Image File Uploader",
+  //   tools: "Node.js, Firebase",
+  //   description:
+  //     "Image Uploading plugin. Upload, view, delete items. Multiple choice support. Loading bar, description field. Supports PNG, JPG, JPEG types.",
+  //   code: "https://github.com/hulchenko/file-uploader",
+  //   demo: "https://vigorous-wright-e2ac02.netlify.app/",
+  // },
+  // {
+  //   src: "./images/carousel/covid-tracker.jpg",
+  //   title: "Covid-19 Tracker",
+  //   tools: "Vue, Tailwind",
+  //   description:
+  //     "Fetching worldwide current Covid-19 data from https://covid19api.com/. Responsive, with drop down country choice. Data is updated multiple times a day.",
+  //   code: "https://github.com/hulchenko/vue-covid-19",
+  //   demo: "https://relaxed-thompson-7bcbce.netlify.app/",
+  // },
   {
-    src: "./imgs/carousel/2.jpg",
-    title: "Image File Uploader",
-    tools: "Node.js, Firebase",
-    description:
-      "Image Uploading plugin. Upload, view, delete items. Multiple choice support. Loading bar, description field. Supports PNG, JPG, JPEG types.",
-    code: "https://github.com/hulchenko/file-uploader",
-    demo: "https://vigorous-wright-e2ac02.netlify.app/",
-  },
-  {
-    src: "./imgs/carousel/3.jpg",
-    title: "Covid-19 Tracker",
-    tools: "Vue, Tailwind",
-    description:
-      "Fetching worldwide current Covid-19 data from https://covid19api.com/. Responsive, with drop down country choice. Data is updated multiple times a day.",
-    code: "https://github.com/hulchenko/vue-covid-19",
-    demo: "https://relaxed-thompson-7bcbce.netlify.app/",
-  },
-  {
-    src: "./imgs/carousel/4.jpg",
+    src: "./images/carousel/etch-a-sketch.jpg",
     title: "Etch-A-Sketch",
     tools: "JS, CSS, HTML",
     description:
@@ -38,7 +48,7 @@ const projects = [
     demo: "https://hulchenko.github.io/etch-a-sketch-project/",
   },
   {
-    src: "./imgs/carousel/5.jpg",
+    src: "./images/carousel/calculator.jpg",
     title: "Calculator",
     tools: "JS, CSS, HTML",
     description:
@@ -46,15 +56,15 @@ const projects = [
     code: "https://github.com/hulchenko/calculator-project",
     demo: "https://hulchenko.github.io/calculator-project/",
   },
-  {
-    src: "./imgs/carousel/6.jpg",
-    title: "Mortgage Calculator",
-    tools: "React, CSS, API",
-    description:
-      "Loan calculator with various calculating functionalities. Friendly daily advises fetched from API.",
-    code: "https://github.com/hulchenko/mortgage-calculator-produce8",
-    demo: "https://hulchenko.github.io/mortgage-calculator-produce8/",
-  },
+  // {
+  //   src: "./images/carousel/mortgage-calculator.jpg",
+  //   title: "Mortgage Calculator",
+  //   tools: "React, CSS, API",
+  //   description:
+  //     "Loan calculator with various calculating functionalities. Friendly daily advises fetched from API.",
+  //   code: "https://github.com/hulchenko/mortgage-calculator",
+  //   demo: "https://hulchenko.github.io/mortgage-calculator",
+  // },
 ];
 
 export const loadProjects = () => {
@@ -80,11 +90,13 @@ export const loadProjects = () => {
     codeLink.className = "carousel-button-one";
     codeLink.href = code;
     codeLink.innerText = "Code";
+    codeLink.target = "_blank";
 
     const demoLink = document.createElement("a");
     demoLink.className = "carousel-button-two";
     demoLink.href = demo;
     demoLink.innerText = "Demo";
+    demoLink.target = "_blank";
 
     div.append(img, h2, h5, p, codeLink, demoLink);
     carousel.appendChild(div);
