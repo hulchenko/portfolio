535b752bcbbe6f2e2c10cdcbe786e40db88787d5
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-10-15 20:59:53 -0300

    generate bg elements dynamically

--- components/intro.js
+++ components/intro.js
@@ -3,12 +3,29 @@ const background = document.getElementById("intro-bg");
 const bio = document.getElementById("bio");
 const img = document.getElementById("profile-pic");
 
+const bgElements = [
+  { top: "12%", left: "42%", duration: "6s" },
+  { top: "70%", left: "50%", duration: "7s" },
+  { top: "17%", left: "6%", duration: "8s" },
+  { top: "20%", left: "60%", duration: "15s" },
+  { top: "67%", left: "10%", duration: "6s" },
+  { top: "80%", left: "70%", duration: "12s" },
+  { top: "60%", left: "80%", duration: "15s" },
+  { top: "32%", left: "25%", duration: "16s" },
+  { top: "90%", left: "25%", duration: "9s" },
+  { top: "20%", left: "80%", duration: "5s" },
+];
+
 const fillBackground = () => {
-  let count = 0;
-  while (count < 10) {
+  for (const element of bgElements) {
+    const { top, left, duration } = element;
     const el = document.createElement("div");
+
+    el.style.top = top;
+    el.style.left = left;
+    el.style.animation = `animate ${duration} linear infinite`;
+
     background.append(el);
-    count++;
   }
 };
 
